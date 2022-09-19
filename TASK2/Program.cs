3aea86c4ad1a0c18f7ba0c36fa56bf65daf43af8
Irina <sinius@yandex.ru>
2022-09-19 15:41:21 +0300

    First commit

---
+++ TASK2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+
+Console.Write("Enter first number");
+
+var num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number");
+
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+     Console.WriteLine(num2);
+}
---
+++ TASK4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+
+Console.Write("Enter first number");
+
+var num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number");
+
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number");
+
+var num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
