0135d1fd18e93af94b8fee604898e3dd01d228c3
Irina <sinius@yandex.ru>
2022-09-21 16:11:40 +0300

    First commit

---
+++ TASK10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+int amount = Convert.ToString(threeDigitNumber).Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+}
+
---
+++ TASK15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели,и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите порядковый номер дня недели");
+
+int DayN = Convert.ToInt32(Console.ReadLine());
+
+void Checking (int DayN) {
+  if (DayN == 6 || DayN == 7) {
+  Console.WriteLine("(Да,Выходной)");
+  }
+  else if (DayN < 1 || DayN> 7) {
+    Console.WriteLine("Ошибка");
+  }
+  else Console.WriteLine("(Нет, не выходной)");
+}
+Checking(DayN);
