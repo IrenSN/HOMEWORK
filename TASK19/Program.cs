59d2f499964b80113290b290fab98b6e5b97188e
Irina <sinius@yandex.ru>
2022-09-29 18:49:52 +0300

    NEW FILES

---
+++ TASK19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+
+Console. WriteLine("Enter five-digit number");
+
+string number = Console.ReadLine();
+
+void Verify(string number)
+{
+    if (number[0]==number[4] || number[1]== number[3])
+  {
+    Console.WriteLine($"Your number {number} is Palindrom");
+  }
+  else Console.WriteLine($"Your number {number} is not a palindrom");
+}
+if (number.Length== 5)
+{
+  Verify(number);
+}
+else Console.WriteLine($"Enter correct number");
+
+
---
+++ TASK21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 21: Напишите программу, которая принимает 
+ //на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.WriteLine("Enter First point coordinates:");
+double [] x = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())};
+Console.WriteLine("Enter Second point coordinates:");
+double [] y = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())};
+
+double oper = Math.Pow((y[0]-x[0]),3) + Math.Pow((y[1]-x[1]),3) + Math.Pow((y[2]-x[2]),3);
+Console.WriteLine(Math.Sqrt(oper));
