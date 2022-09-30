43968c0483aad7b9275edafcd6114062a8e44124
Irina <sinius@yandex.ru>
2022-09-30 14:04:24 +0300

    Next commit Less4

---
+++ TASK27/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = Length(number);
+Sum(number, length);
+
+int Length(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void Sum(int n, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"The sum is:" + sum);
+}
---
+++ TASK29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+Console.Write("Enter array:");
+
+int array = Convert.ToInt32(Console.ReadLine());
+
+int[] someArray = new int[array];
+
+for (int i = 0; i < someArray.Length; i++)
+{
+    someArray[i] = new Random().Next(1,100);
+
+    Console.Write(someArray[i] + "/ ");
+}
+
